95699162f0cd147ef9d3f50f310807f3b20a4a81
Jake Vis <visser.jacob@outlook.com>
2021-02-22 13:52:57 -0800

    Update ecowitt.py
    Fixing order

--- ecowitt.py
+++ ecowitt.py
@@ -813,10 +813,6 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         body = self.rfile.read(content_length)
-        self.send_response(200)
-        self.end_headers()
-        response = BytesIO()
-        response.write=(b'Ok')
         # response.write(b'This is POST request. ')
         # response.write(b'Received: ')
         # response.write(body)
@@ -824,6 +820,11 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         # print(self.raw_requestline)
         print("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
             str(self.path), str(self.headers), body.decode('utf-8'))
+        
+        response = BytesIO()
+        response.write=(b'Ok')
+        self.send_response(200)
+        self.end_headers()
         
         params = dict([p.split('=') for p in body.decode('utf-8').split('&')])
         control.add_nodes(params)
