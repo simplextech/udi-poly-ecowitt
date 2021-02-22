ed79e5a20e04fdf0c41d503b2ca4a1dce64e8774
Jake Vis <visser.jacob@outlook.com>
2021-02-22 13:12:28 -0800

    Update ecowitt.py

--- ecowitt.py
+++ ecowitt.py
@@ -813,7 +813,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         body = self.rfile.read(content_length)
-        # self.send_response(200)
+        self.send_response(200)
         # self.end_headers()
         # response = BytesIO()
         # response.write(b'This is POST request. ')
